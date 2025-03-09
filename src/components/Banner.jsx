b0306180097ce8d64cb37a9eaed4fac5025ed462
trungdo3224 <trungdo3224@gmail.com>
2025-03-09 21:01:24 +0700

    Update banner images

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = () => {
   return (
-    <div className='banner-bg py-8 relative h-60 md:h-[500px] lg:h-[600px] w-full overflow-hidden md:bg-center md:bg-no-repeat'>
+    <div className='relative w-full overflow-hidden md:bg-center md:bg-no-repeat'>
       <img src='images/atti.webp' alt='banner' className='object-cover' />
     </div>
   );
--- src/components/PromotionBanners.jsx
+++ src/components/PromotionBanners.jsx
@@ -3,10 +3,21 @@ import React from 'react';
 const PromotionBanners = () => {
   return (
     <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 mb-12 lg:mb-0 h-auto lg:h-96'>
-      <div className='h-48 md:h-64 lg:h-full rounded-lg bg-[url(images/fpt-wifi-6-ket-noi-sieu-vuot-troi.jpg)] bg-cover bg-center bg-no-repeat'></div>
+      <div className='h-48 md:h-64 lg:h-full rounded-lg'>
+        <img
+          src='images/fpt-wifi-6-ket-noi-sieu-vuot-troi.jpg'
+          alt='Đăng ký lắp đặt internet'
+          className='w-full h-full object-cover rounded-lg'
+        />
+      </div>
 
       <div className='grid grid-rows-2 gap-4 h-auto lg:h-full'>
-        <div className='h-48 md:h-64 lg:h-full rounded-lg bg-[url(images/tiktok-fpt-live-qua-bay-ve-nha.jpg)] bg-cover bg-center bg-no-repeat'>
+        <div className='h-48 md:h-64 lg:h-full rounded-lg'>
+          <img
+            src='images/tiktok-fpt-live-qua-bay-ve-nha.jpg'
+            alt='Đăng ký lắp đặt internet'
+            className='w-full h-full object-cover rounded-lg'
+          />
         </div>
 
         <div className='h-48 md:h-64 lg:h-full rounded-lg'>
