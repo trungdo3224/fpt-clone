301ad6ea5336ab7509c4ed2f15f85258eef7933c
trungdo3224 <trungdo3224@gmail.com>
2025-03-09 20:53:19 +0700

    update banner image import

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Banner = () => {
   return (
-    <div className='banner-bg py-8 relative h-60 md:h-[500px] lg:h-[600px] w-full overflow-hidden bg-[url(images/attysangloiketnoifptm.png)] md:bg-[url(images/attysangloiketnoifptm.png)] md:bg-center md:bg-no-repeat md:bg-cover xl:bg-[url(images/atti.webp)] bg-center bg-no-repeat bg-cover'></div>
+    <div className='banner-bg py-8 relative h-60 md:h-[500px] lg:h-[600px] w-full overflow-hidden md:bg-center md:bg-no-repeat'>
+      <img src='images/atti.webp' alt='banner' className='object-cover' />
+    </div>
   );
 };
 
